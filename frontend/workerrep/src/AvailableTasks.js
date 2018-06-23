7e866dfb733670d00a886e492752df4fd019f887
Shubham Gupta <shubhamg@iiitd.ac.in>
2018-06-23 20:05:24 +0530

    added react FE

---
+++ frontend/workerrep/src/AvailableTasks.js
@@ -0,0 +1,50 @@
+
+import React, { Component } from 'react';
+import web3 from './web3';
+import storehash from './storehash';
+import Tasklets from './Tasklets';
+
+class AvailableTasks extends Component {
+	constructor (props) {
+super(props);
+ this.state = {
+ 	avail_tasks_id: []
+ }
+
+ this.load_Avail_Tasks(this);
+
+}
+
+
+load_Avail_Tasks = async (this1) => {
+	console.log("loading available tasks");
+
+	const accounts = await web3.eth.getAccounts();
+	     
+	console.log('Sending from Metamask account: ' + accounts[0]);
+	storehash.methods.showAvailableTasks().call({from: accounts[0] })
+.then(function(result){
+    console.log(result);
+    	this1.setState({avail_tasks_id: result});
+    });//function(result)
+}
+
+render()
+{	
+
+	var TaskletList = this.state.avail_tasks_id.map(function(id){
+                        return <Tasklets taskId={id}/>;
+                      })
+
+	return(<div>{TaskletList}</div>
+
+		
+
+
+			
+
+		);
+}
+}
+
+export default AvailableTasks;
---
+++ frontend/workerrep/src/titlebar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { createMuiTheme } from '@material-ui/core/styles';
+
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -6,
+    marginRight: 30,
+  },
+
+ 
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+        
+          <Typography variant="display2" color="inherit" className={classes.flex}>
+            WorkerRep
+
+          </Typography>
+
+          <Button color="inherit">Register</Button>
+        
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
